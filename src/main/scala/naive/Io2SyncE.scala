4c354287e99eb9c8cc87bdf756ba213778ee5af0
leigh-perry <lperry.breakpoint@gmail.com>
2020-01-14 14:56:11 +1100

    SyncE

--- src/main/scala/naive/Io2SyncE.scala
+++ src/main/scala/naive/Io2SyncE.scala
@@ -1,6 +1,6 @@
 package naive
 
-sealed class SyncE[E, A](val unsafeRunSync: () => Either[E, A]) {
+sealed class SyncE[E, A](val unsafeRunEither: () => Either[E, A]) {
   def map[B](f: A => B): SyncE[E, B] =
     flatMap(a => SyncE.pure(f(a)))
 
@@ -8,15 +8,33 @@ sealed class SyncE[E, A](val unsafeRunSync: () => Either[E, A]) {
     new SyncE(
       () =>
         for {
-          a <- unsafeRunSync()
-          b <- f(a).unsafeRunSync()
+          a <- unsafeRunEither()
+          b <- f(a).unsafeRunEither()
         } yield b
     )
 
+  def flatten(s: SyncE[E, SyncE[E, A]]): SyncE[E, A] =
+    s.flatMap(identity)
+
   def attempt: SyncE[Nothing, Either[E, A]] =
     new SyncE(
-      () => Right(unsafeRunSync())
+      () => Right(unsafeRunEither())
     )
+
+  def foldM[B](failure: E => SyncE[E, B], success: A => SyncE[E, B]): SyncE[E, B] =
+    SyncE.pure(()).flatMap {
+      _ =>
+        unsafeRunEither() match {
+          case Left(e) => failure(e)
+          case Right(a) => success(a)
+        }
+    }
+
+  def unsafeRunSync(): A =
+    unsafeRunEither() match {
+      case Left(e) => throw new RuntimeException(e.toString)
+      case Right(a) => a
+    }
 }
 
 object SyncE {
@@ -38,7 +56,14 @@ object SyncEApp {
         b <- SyncE.pure[AppError, String]("b")
       } yield a + b
 
+    val foldMedSuccess =
+      programSuccess.foldM[Boolean](
+        _ => SyncE.pure(false),
+        _ => SyncE.pure(true)
+      )
+
     println(programSuccess.unsafeRunSync())
+    println(foldMedSuccess.unsafeRunSync())
     println(programSuccess.attempt.unsafeRunSync())
 
     val programFailure =
@@ -47,7 +72,13 @@ object SyncEApp {
         b <- SyncE.fail[AppError, String](AppFail("oops"))
       } yield a + b
 
-    println(programFailure.unsafeRunSync())
+    val foldMedFailure =
+      programFailure.foldM[Boolean](
+        _ => SyncE.pure(false),
+        _ => SyncE.pure(true)
+      )
+
+    println(foldMedFailure.unsafeRunSync())
     println(programFailure.attempt.unsafeRunSync())
   }
 }
