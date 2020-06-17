3385cf9896d88d6912e21ddc458fcbb9badd79a8
leigh-perry <lperry.breakpoint@gmail.com>
2020-06-17 18:02:53 +1000

    Tony optics blog part 1

---
+++ modules/misc/src/main/scala/exp/TonyOptics.scala
@@ -0,0 +1,57 @@
+package exp
+
+import cats._
+import cats.data._
+import cats.implicits._
+
+trait Optic[~>[_, _], F[_], A, B] {
+  def run: (B ~> F[B]) => A ~> F[A]
+}
+
+object Optic {
+  def get[A, B](optic: Optic[Function1, Const[B, *], A, B]): A => B =
+    a => optic.run(b => Const(b))(a).getConst
+
+  def set[A, B](optic: Optic[Function1, Id, A, B]): A => B => A =
+    a => b => optic.run(_ => b)(a)
+}
+
+////
+
+final case class Person(age: Int, name: String)
+object Person {
+
+  def age[F[_]: Functor]: Optic[Function1, F, Person, Int] =
+    new Optic[Function1, F, Person, Int] {
+      def run =
+        k =>
+          person => {
+            k(person.age)
+              .map(a => Person(a, person.name))
+          }
+    }
+
+  def name[F[_]: Functor]: Optic[Function1, F, Person, String] =
+    new Optic[Function1, F, Person, String] {
+      def run =
+        k =>
+          person => {
+            k(person.name)
+              .map(n => Person(person.age, n))
+          }
+    }
+
+  def main(args: Array[String]): Unit = {
+    import Optic._
+
+    val p0 = Person(45, "Fred")
+    println(get[Person, Int](age)(p0)) // 45
+    println(get[Person, String](name)(p0)) // Fred
+
+    val p1 = set[Person, Int](age)(p0)(46)
+    println(p1) // Person(46,Fred)
+
+    val p2 = set[Person, String](name)(p1)("Mary")
+    println(p2) // Person(46,Mary)
+  }
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,2 @@
 addSbtPlugin("com.github.cb372" % "sbt-explicit-dependencies" % "0.2.12")
-addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.3.1")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.0")
